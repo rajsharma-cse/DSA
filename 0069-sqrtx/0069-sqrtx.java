62e5356782d4d148e2c2eb84d802266cea75af83
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-07-29 12:53:04 +0530

    Time: 1 ms (97.43%), Space: 42.5 MB (79.24%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mySqrt(int x) {
+        long low=1,high=x;
+        long ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(mid*mid <= x){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return (int)ans;
+    }
+}
