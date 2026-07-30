5a90c8d2ba76ce1f5afcaa5ab3b7a0f4b9cd4ae2
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-07-30 10:33:00 +0530

    Time: 53 ms (5.34%), Space: 91.9 MB (83.51%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean possible(int[] arr,long k,long mid){
+        long count=0;
+        for(int ele:arr){
+            if(ele>=mid){
+                count+=ele/mid;
+            }
+        }
+        return count >= k;
+    }
+    public int maximumCandies(int[] arr, long k) {
+        long low=1L,high=12000000000L;
+        long ans=0;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(possible(arr,k,mid)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return (int)ans;
+    }
+}
