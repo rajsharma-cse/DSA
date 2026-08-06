9a92371dd505a0d5fa55a1b0441c5f81ef53b051
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-08-06 11:01:37 +0530

    Time: 32 ms (16.71%), Space: 48.8 MB (79.53%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int fun(int[] h){
+        int n=h.length;
+        int[] next=new int[n];
+        Stack<Integer> stk=new Stack<>();
+        next[n-1]=n;
+        stk.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(!stk.isEmpty() && h[stk.peek()] >= h[i]) stk.pop();
+            next[i]=stk.isEmpty()?n:stk.peek();
+            stk.push(i);
+        }
+        stk.clear();
+        stk.push(0);
+        int[] prev=new int[n];
+        prev[0]=-1;
+        for(int i=1;i<n;i++){
+            while(!stk.isEmpty() && h[stk.peek()] >= h[i]) stk.pop();
+            prev[i]=stk.isEmpty()?-1:stk.peek();
+            stk.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=Math.max(ans,h[i]*(next[i]-prev[i]-1));
+        }
+        return ans;
+    }
+    public int maximalRectangle(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int[] ans=new int[m];
+        
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='0'){
+                    ans[j]=0;
+                }else ans[j]++;
+            }
+            maxi=Math.max(maxi,fun(ans));
+        }
+        return maxi;
+    }
+}
