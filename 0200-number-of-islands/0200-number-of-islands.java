e4e301b0becf4314557e05e7bd968b27f1dcaf0c
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-08-12 20:05:10 +0530

    Time: 4 ms (46.04%), Space: 52.3 MB (59.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,35 +1,32 @@
 class Solution {
-    public boolean isSafe(int i,int j,int n,int m){
+    int[] x={-1,1,0,0};
+    int[] y={0,0,-1,1};
+    public boolean isSafe(int i,int j,int n, int m){
         return i>=0 && j>=0 && i<n && j<m;
     }
-    public void dfs(int i,int j,int n,int m,char[][] grid,boolean[][] vis){
+    public void dfs(char[][] grid,int i,int j,int n,int m,boolean[][] vis){
         vis[i][j]=true;
-        if(isSafe(i+1,j,n,m) && grid[i+1][j]=='1' && !vis[i+1][j])
-            dfs(i+1,j,n,m,grid,vis);
-
-        if(isSafe(i,j+1,n,m) && grid[i][j+1]=='1' && !vis[i][j+1])
-            dfs(i,j+1,n,m,grid,vis);
-
-        if(isSafe(i-1,j,n,m) && grid[i-1][j]=='1' && !vis[i-1][j])
-            dfs(i-1,j,n,m,grid,vis);
-
-        if(isSafe(i,j-1,n,m) && grid[i][j-1]=='1' && !vis[i][j-1])
-            dfs(i,j-1,n,m,grid,vis);
+        for(int k=0;k<4;k++){
+            int row=i+x[k];
+            int col=j+y[k];
+            if(isSafe(row,col,n,m) && grid[row][col] == '1' && !vis[row][col]){
+                dfs(grid,row,col,n,m,vis);
+            }
+        }
     }
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-
         boolean[][] vis=new boolean[n][m];
-        int ans=0;
+        int res=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    ans++;
-                    dfs(i,j,n,m,grid,vis);
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    dfs(grid,i,j,n,m,vis);
+                    res++;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
