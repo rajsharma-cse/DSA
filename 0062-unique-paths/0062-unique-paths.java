b0866bdd0197c66b0f1bf7e144a37acfde266be0
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-09-02 21:33:11 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -12,7 +12,15 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         dp=new int[m+1][n+1];
-        for(int row[]:dp) Arrays.fill(row,-1);
-        return fun(0,0,m,n);
+        // for(int row[]:dp) Arrays.fill(row,-1);
+        // return fun(0,0,m,n);
+
+        dp[m-1][n]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
