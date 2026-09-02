b7c8988bfd4c4180fdf3644c112628391213a173
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-09-02 08:53:35 +0530

    Time: 42 ms (49.18%), Space: 46.2 MB (57.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -6,7 +6,7 @@ class Solution {
 
         int res=1;
         for(int i=1;i<n;i++){
-            for(int j=i;j>=0;j--){
+            for(int j=i-1;j>=0;j--){
                 if(arr[j]<arr[i]){
                     dp[i]=Math.max(dp[i],dp[j]+1);
                 }
