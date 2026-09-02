714fff01afaa3831e3127e6c62350b2ffb35b700
rajsharma-cse <rajsharma.cse01@gmail.com>
2026-09-02 21:34:36 +0530

    Time: 1 ms (14.07%), Space: 42.4 MB (12.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -15,9 +15,10 @@ class Solution {
         // for(int row[]:dp) Arrays.fill(row,-1);
         // return fun(0,0,m,n);
 
-        dp[m-1][n]=1;
+        dp[m-1][n-1]=1;
         for(int i=m-1;i>=0;i--){
             for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1) continue;
                 dp[i][j]=dp[i+1][j]+dp[i][j+1];
             }
         }
